94a5faec277dfa189527982a59eeb65af83b4b6a
wooseok <80329046+Low1ife@users.noreply.github.com>
2022-02-11 18:23:43 +0900

    Create constructor.js

---
+++ part2/constructor.js
@@ -0,0 +1,12 @@
+{
+    function shop(pro1 , pro2){
+        this.name = pro1;
+        this.price = pro2;
+    }
+    
+    let product1 = new shop('shirts' , 50000)
+    let product2 = new shop('shoews' , 70000)
+
+    //console.log(product1);
+    //console.log(product2);
+}
