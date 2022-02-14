6f15b5794aa0d7c96f102d09232653288f6c0b48
wooseok <80329046+Low1ife@users.noreply.github.com>
2022-02-14 16:35:36 +0900

    Update constructor.js

--- part2/constructor.js
+++ part2/constructor.js
@@ -1,12 +1,17 @@
 {
+    // prototype & constructor
+
     function shop(pro1 , pro2){
         this.name = pro1;
         this.price = pro2;
     }
     
+    shop.prototype.category = 'cloth';
+
     let product1 = new shop('shirts' , 50000)
     let product2 = new shop('shoews' , 70000)
 
     //console.log(product1);
     //console.log(product2);
+    console.log(product1.category)
 }
