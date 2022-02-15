df6caa28cec4aef4dbfbb3ea6277d3df77ed737a
wooseok <80329046+Low1ife@users.noreply.github.com>
2022-02-15 11:27:58 +0900

    Update constructor.js

--- part2/constructor.js
+++ part2/constructor.js
@@ -13,5 +13,38 @@
 
     //console.log(product1);
     //console.log(product2);
-    console.log(product1.category)
+    //console.log(product1.category)
+    //console.log(product1.__proto__)
+}
+
+{
+    // practice
+
+    function Student(name , age){
+        this.nick = name;
+        this.summer = age;
+    }
+
+    Student.prototype.sayHi = function(){
+        //console.log(`안녕 나는 ${this.nick} 이야`)
+    }
+
+    var student1 = new Student('kim' , 20)
+
+    student1.sayHi();
+}
+
+{
+    Array.prototype.remove3 = function(){
+        this.forEach((el) =>{
+            if(this[el] === 3){
+                this.splice(el , 1)
+            }
+        })
+    }
+
+    let arr = [1,2,3]; // --> new Array(1,2,3)
+
+    arr.remove3();
+    console.log(arr);
 }
